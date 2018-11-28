4385a57856b7d9f41fb61bf6a6a8489b96ba51e5
John Morris <johnmorris4@bellsouth.net>
2018-11-28 08:53:44 -0500

    Found solution to getting the Tomcat Server running. Added the first of the servlets

---
+++ src/main/java/com/morrisje/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.morrisje.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.morrisje")
+public class WebConfig {
+}
